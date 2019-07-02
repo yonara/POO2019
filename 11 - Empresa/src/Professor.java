4a08a397a92392e725877ed9a89e465da54904bc
Yonara Maria Lima Damasceno <yonaradamasceno@alu.ufc.br>
2019-07-01 23:01:03 -0300

    Atualizando

--- 11 - Empresa/src/Professor.java
+++ 11 - Empresa/src/Professor.java
@@ -1,4 +1,28 @@
+class Professor extends Funcionario {
+	
+	char classe;
+	
+	public Professor(String nome,char classe) {
+		super(nome,0);
+		this.classe = classe;
+	}
 
-public class Professor {
+	public String toString() {
+		return "Professor: "+ super.toString() + "\nclasse: " + classe + "\nsalario: "+this.calc_salario();
+	}
+	public double calc_salario() {
+		double salario =(classe - 'A')* 2000 + 3000;
+		if(max_diarias <= 2) {
+			qtd_diarias +=100;
+			System.out.println(" gggg");
+		}
+		return salario;
+	 } 
+		
+	
+	
+	public double setBonus() {
+		 return this.calc_salario() + super.bonus;
+	}
 
 }
