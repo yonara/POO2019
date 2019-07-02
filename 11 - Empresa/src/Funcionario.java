753d8c84c7d63cd2c859e97509cc4dd78646fc61
Yonara Maria Lima Damasceno <yonaradamasceno@alu.ufc.br>
2019-07-01 23:00:00 -0300

    Atualizando

--- 11 - Empresa/src/Funcionario.java
+++ 11 - Empresa/src/Funcionario.java
@@ -1,4 +1,32 @@
-
-public class Funcionario {
-
+abstract class Funcionario {
+	 
+	 protected String nome;
+	 public int qtd_diarias;
+	 public int max_diarias;
+	 protected double bonus;
+	
+	 abstract double calc_salario();
+	
+	 
+	 public Funcionario(String nome,int max_diarias) {
+		 
+		 this.nome = nome;
+		 this.max_diarias = max_diarias;
+		 this.qtd_diarias = qtd_diarias;
+		 this.bonus = bonus;
+	}
+	 public String toString() {
+		 return  nome;
+	 }
+	 public double setBonus() {
+		 return this.bonus/3;
+	 }
+	 public void add_diarias() {
+		 if(qtd_diarias <= 2) {
+			 qtd_diarias += 1;
+			 max_diarias += 100;
+			 
+		 }
+	 }
+	
 }
